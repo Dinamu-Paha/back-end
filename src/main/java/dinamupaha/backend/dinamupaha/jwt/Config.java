acffbecdaf3bcd3675618053534ec727b4c955c0
DinethTharushan <2019cs165@stu.ucsc.lk>
2022-09-07 00:47:38 +0530

    corrections of image uploading process

--- src/main/java/dinamupaha/backend/dinamupaha/jwt/Config.java
+++ src/main/java/dinamupaha/backend/dinamupaha/jwt/Config.java
@@ -53,7 +53,7 @@ public class Config extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/user/login", "/user/signup", "/user/forgotPassword", "/sendmail", "/user/mailverify", "/file/upload")
+                .antMatchers("/user/login", "/user/signup", "/user/forgotPassword", "/sendmail", "/user/mailverify", "/upload/image", "/get/image/info/{name}", "/get/image/{name}")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
