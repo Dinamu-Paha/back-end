4552e1dcb70c0f39defc4bf8cba9e58e55db4a7c
DinethTharushan <2019cs165@stu.ucsc.lk>
2022-09-07 00:48:30 +0530

    files of image uploading process

---
+++ src/main/java/dinamupaha/backend/dinamupaha/repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package dinamupaha.backend.dinamupaha.repository;
+
+import java.util.Optional;
+
+import dinamupaha.backend.dinamupaha.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+	Optional<Image> findByName(String name);
+}
